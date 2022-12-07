2d9d54f6be49dde58514ff2b1197ff11361132f5
Hasanuzzaman92 <hasanuzzamanmethu@gmail.com>
2022-12-08 02:20:46 +0600

    react pdf setup

--- src/components/Shared/Courses/CourseDetails.js
+++ src/components/Shared/Courses/CourseDetails.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import { FaDownload, FaStar } from "react-icons/fa";
+import Pdf from "react-to-pdf";
+
+const ref = React.createRef();
 
 const CourseDetails = () => {
   const courseDetail = useLoaderData();
   const { name, id, price, img, rating, desc } = courseDetail;
   return (
-    <div className="w-[80%] mx-auto border-2 p-10 my-20 shadow-xl">
+    <div>
+     
+      <div ref={ref} className="w-[80%] mx-auto border-2 p-10 my-20 shadow-xl">
       <div className="flex justify-between items-center">
         <h2 className="text-4xl font-semibold">
           Course Name: <span className="text-rose-500 italic">{name}</span>{" "}
         </h2>
-        <FaDownload className="text-4xl cursor-pointer"></FaDownload>
+        <Pdf targetRef={ref} filename={`${name}.pdf`}>
+        {({ toPdf }) => <button onClick={toPdf}><FaDownload className="text-4xl cursor-pointer"></FaDownload></button>}
+      </Pdf>
+        
       </div>
       <figure className="mt-10 ">
         <img className="w-[80%] mx-auto" src={img} alt="" />
@@ -27,6 +35,7 @@ const CourseDetails = () => {
       <div className="mt-10 text-center">
         <Link to={`/checkout/${id}`}> <button className="btn btn-outline btn-error">Get Premium Access</button> </Link>
       </div>
+    </div>
     </div>
   );
 };
